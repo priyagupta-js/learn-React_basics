0f76361bd3323947e1f5da9f57fcdd822e02ccf5
Priya Gupta <priyagupta052k@gmail.com>
2024-02-17 23:55:02 +0530

    React Props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import JSXCode from "../src/Practice/JSXCode";
 import ReactStyling from "../src/Practice/ReactStyling";
+import Properties from "./Practice/Properties";
 
 function App() {
   return (
     <div className="App">
     <JSXCode />
      <ReactStyling />
+     <Properties />
     </div>
   );
 }
